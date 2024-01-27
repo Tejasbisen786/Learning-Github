20585af0da6b176f78210fd759cff2cee0ca82e6
Tejasbisen786 <bisentejas@gmail.com>
2024-01-27 20:22:58 +0530

    added is19 fun

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const myName="Tejas"
     console.log(myName);
  }
  getName();
- const age= 21
+ const age= 19
  const dob= "22 June 2002"
  const name="Tejas Bisen"
+
+ function isAbove19(){
+   return age>=19
+ }
