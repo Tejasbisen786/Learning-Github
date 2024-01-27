c7494e5ce40fdbc8bf78b45d284d0ea1d240b2e9
Tejasbisen786 <bisentejas@gmail.com>
2024-01-27 20:52:17 +0530

    reverting fn 18

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const myName="Tejas"
  const dob= "22 June 2002"
  const name="Tejas Bisen"
 
+<<<<<<< HEAD
 
  function newFn(){
    console.log("Hey im new function ");
+=======
+ function isAbove19(){
+   return age>=19
+>>>>>>> parent of 56e7a29 (remove 19 fn)
  }
